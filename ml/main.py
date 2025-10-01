c7a0b1c148491eb40f005cd033dbc8a0f9df26fe
danil212211 <danil.zelenskiy.2005@mail.ru>
2025-10-01 21:13:15 +0900

    test 3000 points

--- ml/main.py
+++ ml/main.py
@@ -6,28 +6,39 @@ from anomaly_detection.streaming_anomaly_service import AnomalyService
 svc = ForecastService(model_dir="forecast/artifacts")
 
 det = AnomalyService(model_dir="anomaly_detection/artifacts")
+import time
 
+def current_milli_time():
+    return round(time.time() * 1000)
 msg = ""
 
-for i in range(25):
-    det.process_message("0,85.5,12.3")
-    msg += "0,85.5,12.3\n"
+for i in range(6000):
+    det.process_message(f"{i},85.5,12.3")
+    msg += f"{i},85.5,12.3\n"
 
 
-K = 5 # прогноз на 5 шагов вперёд
+K = 3000 # прогноз на 3000 шагов вперёд
 
 
+
+now = current_milli_time()
 out = svc.process_message(msg, K)
 
 if out["ready"] == True:
-    print(out["forecast"])
+
+    print(current_milli_time() - now)
+    print(len(out["forecast"].split('\n')))
     pass
 else:
     print(f"Need {out["needed"]} more points before forecasting can start")  
 
+
+now = current_milli_time()
 out = det.process_message("0,85.5,12.3")
 
 if out["ready"]:
+
+    print(current_milli_time() - now)
      # timestamp,group_id,sequence_id,bpm,uterus,bpm_anomaly,uterus_anomaly,anomaly_score
     print(out["detection"])
 else:
