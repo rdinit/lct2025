fb6e47ce79e6a059cc1e310e54db68855e91a900
rdinit <28570294+rdinit@users.noreply.github.com>
2025-10-02 03:27:03 +0300

    better structure

--- backend/main_back/cmd/main/main.go
+++ backend/main_back/cmd/main/main.go
@@ -3,197 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
+	"main_back/internal/datamanagers"
 	"math/rand"
 	"net/http"
-	"strconv"
-	"strings"
-	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-/*
-type DataChunk struct {
-	data []DataPoint
-	mu   sync.RWMutex
-}
-*/
-
-type DataPoint struct {
-	Time  float64
-	Value float64
-}
-
-type DataManager struct {
-	Id                string
-	outputConnections []*websocket.Conn
-	mu                sync.RWMutex
-	data              []DataPoint
-	//dataChunks []DataChunk
-	//marks []Marks
-}
-
-func NewDataManager() *DataManager {
-	return &DataManager{
-		data: make([]DataPoint, 0),
-	}
-}
-
-func (dm *DataManager) AddOutputConnection(c *websocket.Conn) {
-	dm.outputConnections = append(dm.outputConnections, c)
-}
-
-func (dm *DataManager) RemoveOutputConnection(c *websocket.Conn) {
-	for i, conn := range dm.outputConnections {
-		if conn == c {
-			dm.outputConnections = append(dm.outputConnections[:i], dm.outputConnections[i+1:]...)
-			break
-		}
-	}
-}
-
-func (dm *DataManager) AppendData(dp DataPoint) {
-	dm.mu.Lock()
-	defer dm.mu.Unlock()
-	dm.data = append(dm.data, dp)
-}
-
-func (dm *DataManager) SendUpdate(message []byte) {
-	for _, conn := range dm.outputConnections {
-		conn.WriteMessage(websocket.TextMessage, message)
-	}
-}
-
-func (dm *DataManager) ProcessMessage(message []byte) {
-	splitted := strings.Split(string(message), ",")
-	time, _ := strconv.ParseFloat(splitted[0], 64)
-	value, _ := strconv.ParseFloat(splitted[1], 64)
-	dp := DataPoint{time, value}
-
-	dm.SendUpdate(message)
-	dm.AppendData(dp)
-}
-
-type MergedPoint struct {
-	Time   float64
-	Bpm    float64
-	Uterus float64
-}
-
-type MLHandler struct {
-	overlap           int
-	lenght            int
-	last_point        int
-	ml_connection     *websocket.Conn
-	outputConnections []*websocket.Conn
-}
-
-func (m *MLHandler) AddOutputConnection(c *websocket.Conn) {
-	m.outputConnections = append(m.outputConnections, c)
-}
-
-func (m *MLHandler) RemoveOutputConnection(c *websocket.Conn) {
-	for i, conn := range m.outputConnections {
-		if conn == c {
-			m.outputConnections = append(m.outputConnections[:i], m.outputConnections[i+1:]...)
-			break
-		}
-	}
-}
-
-func NewMLHandler(overlap int, length int) *MLHandler {
-	return &MLHandler{overlap, length, 0, nil, make([]*websocket.Conn, 0)}
-}
-
-func (m *MLHandler) AskML(messages []string) {
-	message := []byte(strings.Join(messages, "\n"))
-	m.ml_connection.WriteMessage(websocket.TextMessage, message)
-}
-
-func (m *MLHandler) SendUpdate(message []byte) {
-	for _, conn := range m.outputConnections {
-		conn.WriteMessage(websocket.TextMessage, message)
-	}
-}
-
-type DataMerger struct {
-	bpm_data        []DataPoint
-	uterus_data     []DataPoint
-	merged_data     []MergedPoint
-	merged_data_str []string
-	points_count    int
-	eps             float64
-	ml_handlers     map[string]*MLHandler
-	mu              sync.RWMutex
-}
-
-func NewDataMerger() *DataMerger {
-	return &DataMerger{
-		bpm_data:        make([]DataPoint, 0),
-		uterus_data:     make([]DataPoint, 0),
-		merged_data:     make([]MergedPoint, 0),
-		merged_data_str: make([]string, 0),
-		points_count:    0,
-		eps:             0.05,
-		ml_handlers:     make(map[string]*MLHandler),
-		mu:              sync.RWMutex{},
-	}
-}
-
-func (d *DataMerger) mergedPointToCSV(dp MergedPoint) string {
-	return fmt.Sprintf("%f,%f\n", dp.Time, dp.Bpm, dp.Uterus)
-}
-
-func (d *DataMerger) AppendData(sensor_id string, dp DataPoint) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	switch sensor_id {
-	case "bpm":
-		d.bpm_data = append(d.bpm_data, dp)
-	case "uterus":
-		d.uterus_data = append(d.uterus_data, dp)
-	}
-	// merge data if both have same timestamp
-	for len(d.bpm_data) > 0 && len(d.uterus_data) > 0 {
-		if math.Abs(d.bpm_data[0].Time-d.uterus_data[0].Time) < d.eps {
-			d.merged_data = append(d.merged_data, MergedPoint{d.bpm_data[0].Time, d.bpm_data[0].Value, d.uterus_data[0].Value})
-			d.bpm_data = d.bpm_data[1:]
-			d.uterus_data = d.uterus_data[1:]
-		} else {
-			if d.bpm_data[0].Time < d.uterus_data[0].Time {
-				d.merged_data = append(d.merged_data, MergedPoint{d.bpm_data[0].Time, d.bpm_data[0].Value, 0})
-				d.bpm_data = d.bpm_data[1:]
-			} else {
-				d.merged_data = append(d.merged_data, MergedPoint{d.uterus_data[0].Time, 0, d.uterus_data[0].Value})
-				d.uterus_data = d.uterus_data[1:]
-			}
-		}
-		d.merged_data_str = append(d.merged_data_str, d.mergedPointToCSV(d.merged_data[len(d.merged_data)-1]))
-		d.points_count++
-	}
-
-	for _, ml_handler := range d.ml_handlers {
-		if ml_handler.last_point-ml_handler.overlap+ml_handler.lenght <= int(d.points_count) {
-			ml_handler.last_point = d.points_count
-			ml_handler.AskML(d.merged_data_str[ml_handler.last_point-ml_handler.overlap : ml_handler.last_point-ml_handler.overlap+ml_handler.lenght])
-		}
-	}
-}
-
-func health_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "healthy ", rn)
-}
-
 var rn int
 
-var dataManagers = make(map[string]*DataManager)
+var dataManagers = make(map[string]*datamanagers.DataManager)
 
-var dataMerger = NewDataMerger()
+var dataMerger = datamanagers.NewDataMerger()
 
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool {
 	return true
 }}
+
+func health_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "healthy ", rn)
+}
 
 func new_data_handler(w http.ResponseWriter, r *http.Request) {
 
@@ -248,8 +77,6 @@ func new_reader_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	dataManager := dataManagers[sensor_id]
-	dataManager.mu.Lock()
-	defer dataManager.mu.Unlock()
 
 	c.SetCloseHandler(func(code int, text string) error {
 		dataManager.RemoveOutputConnection(c)
@@ -270,12 +97,12 @@ func get_forecast_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["forecast"].RemoveOutputConnection(c)
+		dataMerger.Ml_handlers["forecast"].RemoveOutputConnection(c)
 		log.Println("disconnected:")
 		return nil
 	})
 
-	dataMerger.ml_handlers["forecast"].AddOutputConnection(c)
+	dataMerger.Ml_handlers["forecast"].AddOutputConnection(c)
 
 }
 
@@ -288,12 +115,12 @@ func get_anomaly_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["anomaly"].RemoveOutputConnection(c)
+		dataMerger.Ml_handlers["anomaly"].RemoveOutputConnection(c)
 		log.Println("disconnected:")
 		return nil
 	})
 
-	dataMerger.ml_handlers["anomaly"].AddOutputConnection(c)
+	dataMerger.Ml_handlers["anomaly"].AddOutputConnection(c)
 }
 func get_classify_handler(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -304,12 +131,12 @@ func get_classify_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["classify"].RemoveOutputConnection(c)
+		dataMerger.Ml_handlers["classify"].RemoveOutputConnection(c)
 		log.Println("disconnected:")
 		return nil
 	})
 
-	dataMerger.ml_handlers["classify"].AddOutputConnection(c)
+	dataMerger.Ml_handlers["classify"].AddOutputConnection(c)
 }
 
 func make_forecast_handler(w http.ResponseWriter, r *http.Request) {
@@ -321,7 +148,7 @@ func make_forecast_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["forecast"].ml_connection = nil
+		dataMerger.Ml_handlers["forecast"].DisconnectML()
 		log.Println("disconnected:")
 		return nil
 	})
@@ -343,7 +170,7 @@ func make_forecast_handler(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 			//log.Println("recieved message: ", string(message))
-			dataMerger.ml_handlers["forecast"].SendUpdate(message)
+			dataMerger.Ml_handlers["forecast"].SendUpdate(message)
 		}
 	}()
 }
@@ -357,7 +184,7 @@ func make_anomaly_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["anomaly"].ml_connection = nil
+		dataMerger.Ml_handlers["anomaly"].DisconnectML()
 		log.Println("disconnected:")
 		return nil
 	})
@@ -379,7 +206,7 @@ func make_anomaly_handler(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 			//log.Println("recieved message: ", string(message))
-			dataMerger.ml_handlers["anomaly"].SendUpdate(message)
+			dataMerger.Ml_handlers["anomaly"].SendUpdate(message)
 		}
 	}()
 }
@@ -393,7 +220,7 @@ func make_classify_handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c.SetCloseHandler(func(code int, text string) error {
-		dataMerger.ml_handlers["classify"].ml_connection = nil
+		dataMerger.Ml_handlers["classify"].DisconnectML()
 		log.Println("disconnected:")
 		return nil
 	})
@@ -415,7 +242,7 @@ func make_classify_handler(w http.ResponseWriter, r *http.Request) {
 				continue
 			}
 			//log.Println("recieved message: ", string(message))
-			dataMerger.ml_handlers["classify"].SendUpdate(message)
+			dataMerger.Ml_handlers["classify"].SendUpdate(message)
 		}
 	}()
 }
@@ -424,8 +251,8 @@ func main() {
 	rn = rand.Intn(100)
 	fmt.Println(rn)
 
-	dataManagers["bpm"] = NewDataManager()
-	dataManagers["uterus"] = NewDataManager()
+	dataManagers["bpm"] = datamanagers.NewDataManager()
+	dataManagers["uterus"] = datamanagers.NewDataManager()
 
 	http.HandleFunc("/health", health_handler)
 	http.HandleFunc("/send", new_data_handler)
---
+++ backend/main_back/internal/datamanagers/datamanagers.go
@@ -0,0 +1,176 @@
+package datamanagers
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type DataPoint struct {
+	Time  float64
+	Value float64
+}
+
+type DataManager struct {
+	Id                string
+	outputConnections []*websocket.Conn
+	mu                sync.RWMutex
+	data              []DataPoint
+	//dataChunks []DataChunk
+	//marks []Marks
+}
+
+func NewDataManager() *DataManager {
+	return &DataManager{
+		data: make([]DataPoint, 0),
+	}
+}
+
+func (dm *DataManager) AddOutputConnection(c *websocket.Conn) {
+	dm.outputConnections = append(dm.outputConnections, c)
+}
+
+func (dm *DataManager) RemoveOutputConnection(c *websocket.Conn) {
+	for i, conn := range dm.outputConnections {
+		if conn == c {
+			dm.outputConnections = append(dm.outputConnections[:i], dm.outputConnections[i+1:]...)
+			break
+		}
+	}
+}
+
+func (dm *DataManager) AppendData(dp DataPoint) {
+	dm.mu.Lock()
+	defer dm.mu.Unlock()
+	dm.data = append(dm.data, dp)
+}
+
+func (dm *DataManager) SendUpdate(message []byte) {
+	for _, conn := range dm.outputConnections {
+		conn.WriteMessage(websocket.TextMessage, message)
+	}
+}
+
+func (dm *DataManager) ProcessMessage(message []byte) {
+	splitted := strings.Split(string(message), ",")
+	time, _ := strconv.ParseFloat(splitted[0], 64)
+	value, _ := strconv.ParseFloat(splitted[1], 64)
+	dp := DataPoint{time, value}
+
+	dm.SendUpdate(message)
+	dm.AppendData(dp)
+}
+
+type MergedPoint struct {
+	Time   float64
+	Bpm    float64
+	Uterus float64
+}
+
+type MLHandler struct {
+	overlap           int
+	lenght            int
+	last_point        int
+	ml_connection     *websocket.Conn
+	outputConnections []*websocket.Conn
+}
+
+func (m *MLHandler) AddOutputConnection(c *websocket.Conn) {
+	m.outputConnections = append(m.outputConnections, c)
+}
+
+func (m *MLHandler) RemoveOutputConnection(c *websocket.Conn) {
+	for i, conn := range m.outputConnections {
+		if conn == c {
+			m.outputConnections = append(m.outputConnections[:i], m.outputConnections[i+1:]...)
+			break
+		}
+	}
+}
+
+func NewMLHandler(overlap int, length int) *MLHandler {
+	return &MLHandler{overlap, length, 0, nil, make([]*websocket.Conn, 0)}
+}
+
+func (m *MLHandler) AskML(messages []string) {
+	message := []byte(strings.Join(messages, "\n"))
+	m.ml_connection.WriteMessage(websocket.TextMessage, message)
+}
+
+func (m *MLHandler) SendUpdate(message []byte) {
+	for _, conn := range m.outputConnections {
+		conn.WriteMessage(websocket.TextMessage, message)
+	}
+}
+
+func (m *MLHandler) DisconnectML() {
+	m.ml_connection = nil
+}
+
+type DataMerger struct {
+	bpm_data        []DataPoint
+	uterus_data     []DataPoint
+	merged_data     []MergedPoint
+	merged_data_str []string
+	points_count    int
+	eps             float64
+	Ml_handlers     map[string]*MLHandler
+	mu              sync.RWMutex
+}
+
+func NewDataMerger() *DataMerger {
+	return &DataMerger{
+		bpm_data:        make([]DataPoint, 0),
+		uterus_data:     make([]DataPoint, 0),
+		merged_data:     make([]MergedPoint, 0),
+		merged_data_str: make([]string, 0),
+		points_count:    0,
+		eps:             0.05,
+		Ml_handlers:     make(map[string]*MLHandler),
+		mu:              sync.RWMutex{},
+	}
+}
+
+func (d *DataMerger) mergedPointToCSV(dp MergedPoint) string {
+	return fmt.Sprintf("%f,%f\n", dp.Time, dp.Bpm, dp.Uterus)
+}
+
+func (d *DataMerger) AppendData(sensor_id string, dp DataPoint) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	switch sensor_id {
+	case "bpm":
+		d.bpm_data = append(d.bpm_data, dp)
+	case "uterus":
+		d.uterus_data = append(d.uterus_data, dp)
+	}
+	// merge data if both have same timestamp
+	for len(d.bpm_data) > 0 && len(d.uterus_data) > 0 {
+		if math.Abs(d.bpm_data[0].Time-d.uterus_data[0].Time) < d.eps {
+			d.merged_data = append(d.merged_data, MergedPoint{d.bpm_data[0].Time, d.bpm_data[0].Value, d.uterus_data[0].Value})
+			d.bpm_data = d.bpm_data[1:]
+			d.uterus_data = d.uterus_data[1:]
+		} else {
+			if d.bpm_data[0].Time < d.uterus_data[0].Time {
+				d.merged_data = append(d.merged_data, MergedPoint{d.bpm_data[0].Time, d.bpm_data[0].Value, 0})
+				d.bpm_data = d.bpm_data[1:]
+			} else {
+				d.merged_data = append(d.merged_data, MergedPoint{d.uterus_data[0].Time, 0, d.uterus_data[0].Value})
+				d.uterus_data = d.uterus_data[1:]
+			}
+		}
+		d.merged_data_str = append(d.merged_data_str, d.mergedPointToCSV(d.merged_data[len(d.merged_data)-1]))
+		d.points_count++
+	}
+
+	for _, ml_handler := range d.Ml_handlers {
+		if ml_handler.last_point-ml_handler.overlap+ml_handler.lenght <= int(d.points_count) {
+			ml_handler.last_point = d.points_count
+			ml_handler.AskML(d.merged_data_str[ml_handler.last_point-ml_handler.overlap : ml_handler.last_point-ml_handler.overlap+ml_handler.lenght])
+		}
+	}
+}
